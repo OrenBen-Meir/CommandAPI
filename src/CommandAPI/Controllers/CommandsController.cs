9eb22a1fa4a6c4c5d1a4634a8d9f159b1437cb15
obmeir <orenbm11@gmail.com>
2021-01-20 02:14:21 -0500

    get all commands

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CommandAPI.Models;
+
 
 
 namespace CommandAPI.Controllers
@@ -11,10 +13,13 @@ namespace CommandAPI.Controllers
     [ApiController]
     public class CommandsController
     {
+        private readonly CommandContext _context;
+        public CommandsController(CommandContext context) => _context = context;
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public ActionResult<IEnumerable<Command>> GetCommandItems()
         {
-            return new string[] { "this", "is", "hard", "coded" };
+            return _context.CommandItems;
         }
     }
 }
